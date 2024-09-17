a3bd0a190fd3c3ab146dfa93dbeafb4feefa378d
hey.its.a <141285328+ageeha927@users.noreply.github.com>
2024-09-17 15:23:13 -0700

    ..

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var fs = require('fs');
-var path = require('path');
-var app = express();
-const PORT = 5000;
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const app = express()
+const PORT = 5000
+const path = require('path')
 
 const eventsData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'events.json'), 'utf8'));
 // set the view to engine ejs
@@ -13,9 +14,9 @@ app.set('view engine', 'ejs')
 
 //index page
 app.get('/', function(req, res) {
+    const events = getEvents()
     res.render('pages/events',{
-        events: eventsData,
-    })
+        events})
 })
 
 // admin page
@@ -23,6 +24,70 @@ app.get('/admin', function(req, res){
     res.render('pages/admin')
 })
 
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+//Load events from the JSON file
+const getEvents = () =>{
+    const eventsData = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'events.json'), 'utf8'))
+    return eventsData
+}
+
+const saveEvents = (events) => {
+    fs.writeFileSync('./data/events.json', JSON.stringify(events, null, 2))
+}
+
+//Routes
+
+//GET: Show all events
+// app.get('/', (req, res)=>{
+//     const events = getEvents()
+//     res.render('events', { events })
+// })
+
+//POST: Create a new event
+app.post('/edit', (req,res) => {
+    const events = getEvents();
+    const newTask = {
+        id: events.length+1,
+        name:req.body.name,
+        date:req.body.date,
+        description: req.body.description
+    }
+    events.push(newTask)
+    saveEvents(events)
+    res.redirect('/')
+})
+
+//GET : Show a single event (for editing)
+app.get('/edit/:id/edit', (req,res)=>{
+    const events = getEvents()
+    const event = events.find(event => event.id == req.params.id)
+    res.render('pages/edit', {event})
+})
+
+//PUT: Update a event
+app.post('/edit/:id', (req,res)=>{
+    const events = getEvents()
+    const eventIndex = events.findIndex(event => event.id == req.params.id)
+    events[eventIndex].description = req.body.description
+    events[eventIndex].name = req.body.name
+    events[eventIndex].date = req.body.date
+    console.log(events[eventIndex])
+    saveEvents(events)
+    res.redirect('/')
+})
+
+//DELETE: Delete a event
+app.post('/edit/:id/delete', (req,res)=> {
+    let events = getEvents()
+    events = events.filter(event => event.id != req.params.id)
+    saveEvents(events)
+    res.redirect('/')
+})
+
+//Server
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 })
